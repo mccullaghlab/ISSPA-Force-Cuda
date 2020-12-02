74847c20a4942663e9f5681ea011008689a3784e
Ryan Weber <ryan.weber@colostate.edu>
2020-12-01 19:18:45 -0600

    optimized the code

--- timing_tests/ADI2/profile.sh
+++ timing_tests/ADI2/profile.sh
@@ -1,1 +1,1 @@
-sudo nvprof --analysis-metrics -o isspa2_analysis.nvprof ../../bin/total_force_cuda.x config.txt
+sudo nvprof -f --analysis-metrics -o isspa2_analysis.nvprof ../../bin/total_force_cuda.x config.txt
