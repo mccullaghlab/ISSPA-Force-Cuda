6dcc769f3a61c0ae74ee779e068aff323e378338
Ryan Weber <ryan.weber@colostate.edu>
2020-12-09 23:53:19 -0600

    including US data

--- timing_tests/ADI2/test.sh
+++ timing_tests/ADI2/test.sh
@@ -2,8 +2,9 @@
 #/bin/bash
 export CUDA_VISIBLE_DEVICES=0
 #/usr/local/cuda-9.2/bin/cuda-memcheck --log-file memcheck.log ../../bin/total_force_cuda.x config.txt  
-../../bin/total_force_cuda.x config.txt > $1_run01.dat
-../../bin/total_force_cuda.x config.txt > $1_run02.dat
-../../bin/total_force_cuda.x config.txt > $1_run03.dat
+../../bin/total_force_cuda.x config.txt
+#../../bin/total_force_cuda.x config.txt > $1_run01.dat
+#../../bin/total_force_cuda.x config.txt > $1_run02.dat
+#../../bin/total_force_cuda.x config.txt > $1_run03.dat
 #../../bin/total_force_cuda.x config.txt > PDI.2.run02.log
   
